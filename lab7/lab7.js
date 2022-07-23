89c195be1cc564030c938c539dd64fca65a79af0
Tsinat <tsinat114476@gmail.com>
2022-07-22 23:54:44 -0500

    w1d4

---
+++ lab7/lab7.js
@@ -0,0 +1,17 @@
+function TaskSaver() {
+    let task = document.getElementById('myInput').value;
+    let words = localStorage.getItem("ibu");
+    if(words!=null){
+     localStorage.setItem("ibu", words + "\n" + task);
+    }
+    else{
+      localStorage.setItem("ibu",  "\n" + task);
+    }
+      tasklist.innerHTML = localStorage.ibu;
+      document.getElementById('myInput').value = null;
+    }
+    function TaskClear() {
+      document.getElementById('myInput').value = null;
+      window.localStorage.clear();
+      location.reload();
+    }
